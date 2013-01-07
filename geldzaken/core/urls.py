cdea92276522871ef43a0d2870636861cb9c2705
Douwe van der Meij <vandermeij@gw20e.com>
2013-01-07 17:05:50 +0100

    readme, bugfix

--- geldzaken/core/urls.py
+++ geldzaken/core/urls.py
@@ -4,8 +4,8 @@ from geldzaken.core import views as core_views
 urlpatterns = patterns('',
     url(r'^$', core_views.YearView.as_view(), name='totaal'),
     url(r'^year/(?P<year>\d{4})/', core_views.YearView.as_view(), name='year'),
-    url(r'^details/(?P<year>\d{4})/(?P<month>\d)/', core_views.DetailView.as_view(), name='details'),
 
     url(r'^json/boeking/(?P<year>\d+)/(?P<month>\d+)/', core_views.BoekingView.as_view(), name='boeking'),
     url(r'^json/categorie/', core_views.CategorieView.as_view(), name='categorie'),
+    url(r'^json/details/(?P<year>\d+)/(?P<month>\d+)/', core_views.DetailView.as_view(), name='details'),
 )
